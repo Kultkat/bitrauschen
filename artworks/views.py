60c8f51efb8ca53543444a3c00702f2570dc4a3d
Kaspar König <studio@kasparkoenig.com>
2025-10-29 11:30:11 +0000

    Add artwork map view and template

--- artworks/views.py
+++ artworks/views.py
@@ -1,8 +1,19 @@
-# artworks/views.py
 from django.shortcuts import render
 from .models import Artwork
+from django.core.serializers import serialize
 
 def artwork_map(request):
-    artworks = list(Artwork.objects.values('title', 'latitude', 'longitude'))
-    return render(request, 'artworks/artwork_map.html', {'artworks': artworks})
-# Create your views here.
+    """
+    View to render the artwork map page.
+    Serializes all artworks to JSON and passes them to the template.
+    """
+    # Query all artworks
+    artworks = Artwork.objects.all()
+
+    # Serialize queryset to JSON, including only specific fields
+    artworks_json = serialize('json', artworks, fields=('title', 'latitude', 'longitude', 'description'))
+
+    # Render the template with artworks JSON
+    return render(request, 'artworks/artwork_map.html', {
+        'artworks': artworks_json
+    })
