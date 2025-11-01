3e21fada8623296993e2822f0c85e21652de22a7
Kaspar König <studio@kasparkoenig.com>
2025-11-01 22:44:00 +0000

    Backup before DB/URL fixes

--- bitrauschen/settings.py
+++ bitrauschen/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
